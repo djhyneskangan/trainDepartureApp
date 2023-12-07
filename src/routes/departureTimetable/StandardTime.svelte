cd3de75ab0cec44e2eb101f820d3f1064167eafb
iicdi23 <faizi642@hotmail.com>
2023-12-07 13:02:51 +1100

    Adding standard time.

---
+++ src/routes/departureTimetable/StandardTime.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { onMount, onDestroy } from 'svelte';
+  
+    let currentTime = new Date();
+  
+    // Update the current time every second
+    const updateCurrentTime = () => {
+      currentTime = new Date();
+    };
+  
+    // Set up interval to update time
+    let interval;
+    onMount(() => {
+      interval = setInterval(updateCurrentTime, 1000);
+    });
+  
+    // Clear interval on component destruction to prevent memory leaks
+    onDestroy(() => {
+      clearInterval(interval);
+    });
+  </script>
+  
+  <div>
+    <h2>AEST:</h2>
+    <p>{currentTime.toLocaleTimeString()}</p>
+  </div>
+
+  
+  
+
