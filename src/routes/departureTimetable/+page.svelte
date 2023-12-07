fda3069ebdfa91dab1ca06fdcf68b18595bf85e3
iicdi23 <faizi642@hotmail.com>
2023-12-07 14:38:25 +1100

    2nd commit

--- src/routes/departureTimetable/+page.svelte
+++ src/routes/departureTimetable/+page.svelte
@@ -2,12 +2,14 @@
 	import { Button } from 'flowbite-svelte';
 	import TrainTimes from './trainTimes.svelte';
 	import Weather from './weather.svelte';
+	import StandardTime from './StandardTime.svelte';
 </script>
 
 <div class="grid grid-cols-4 gap-4 max-w-2xl mx-auto shrink">
-	<div class="col-span-1"><Button><a href="/logout">Logout</a></Button></div>
-	<div class="col-span-3">
-		<Weather />
+	<div class="col-span-3"><Button><a href="/logout">Logout</a></Button></div>
+	<div class="col-span-1">
+		<StandardTime/>
 	</div>
 	<div class="col-span-4"><TrainTimes /></div>
 </div>
+
